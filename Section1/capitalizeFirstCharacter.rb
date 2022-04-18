6771a994f0716c498998c95559c1d3ed01bc897a
Suraj Pai K <53591334+surajpai50612@users.noreply.github.com>
2022-04-18 15:46:05 +0530

    Delete Section1 directory

--- Section1/capitalizeFirstCharacter.rb
+++
@@ -1,8 +0,0 @@
-print "Enter a sentense: "
-sentense=gets.chomp!
-
-# .split used to split sentense by space(' ')
-# .map(&:method) change the actual value and apply method of those
-# .join(' ') joins the words by space(' ')
-
-print "Capitalized sentense is: "+(sentense.split.map(&:capitalize).join(' ')) 
--- Section1/stringToLowerCase.rb
+++
@@ -1,4 +0,0 @@
-print "Enter a string: "
-str2=gets.chomp.downcase!
-
-print "String in lowercase is: "+str2
